e1a7a75e2ee194862accdafc634aa1f937ff02ea
itsmearnab12 <arnabchoudhury2005@gmail.com>
2025-10-13 10:02:00 +0530

    fontend work finished for profile section

--- Chatapp/client/src/pages/ProfilePage.jsx
+++ Chatapp/client/src/pages/ProfilePage.jsx
@@ -10,10 +10,15 @@ const ProfilePage = () => {
   const [name, setName] = useState("Martin Johnson")
   const [bio, setBio] = useState("Hi Everyone, I am using Quickchat")
 
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    navigate('/')
+  }
+
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
      <div className='w-5/6 max-w-2x1 backdrop-blur-2x1 text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
-      <form className="flex flex-col gap-5 p-10 flex-1">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5 p-10 flex-1">
         <h3 className="text-lg">Profile details</h3>
         <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
           <input onChange={(e)=>setSelectedImg(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
