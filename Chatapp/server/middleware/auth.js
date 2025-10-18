5bd507f6166967bb8057dc0f7eec18d00ea88d8a
itsmearnab12 <arnabchoudhury2005@gmail.com>
2025-10-18 16:50:23 +0530

    Work on Login/Signup

--- Chatapp/server/middleware/auth.js
+++ Chatapp/server/middleware/auth.js
@@ -10,8 +10,12 @@ export const protectRoute = async (req, res, next)=>{
 
         const user = await User.findById(decoded.userId).select("-password");
 
-        
+        if(!user) return res.json({ success: false, message: "User not found"});
+
+        req.user = user;
+        next();
     } catch (error) {
-        
+        console.log(error.message);
+        res.json({ success: false, message: error.message});
     }
 }
